ed42701eb1b162c3a0477558ed650e64895d9fc7
Beau Coker <beau.coker@gmail.com>
2018-11-02 01:07:32 -0400

    initial commit

---
+++ shell_1.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH -c 1                               # Request one core
+#SBATCH -N 1                               # Request one node (if you request more than one core with -c, also using
+#SBATCH -t 0-00:20                         # Runtime in D-HH:MM format
+#SBATCH -p short                           # Partition to run in
+#SBATCH --mem=100                          # Memory total in MB (for all cores)
+#SBATCH -o hostname_%j.out                 # File to which STDOUT will be written, including job ID
+#SBATCH -e hostname_%j.err                 # File to which STDERR will be written, including job ID
+#SBATCH --mail-type=ALL                    # Type of email notification- BEGIN,END,FAIL,ALL
+#SBATCH --mail-user=beaucoker@g.harvard.edu   # Email to which notifications will be sent
+
+source myvirtualenv2/bin/activate
+
+python3 train_tcga.py config/tcga.yaml
