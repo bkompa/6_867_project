b49f4f556992553c2de45a5df99b2e1cd2eac61a
Beau Coker <beau.coker@gmail.com>
2018-11-05 15:56:09 -0500

    git finally working

--- shell_1_gpu.sh
+++ shell_1_gpu.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-#SBATCH -c 1                               # Request one core
+#SBATCH -c 4                               # Request one core
 #SBATCH -N 1                               # Request one node (if you request more than one core with -c, also using
-#SBATCH -t 0-00:10                         # Runtime in D-HH:MM format
+#SBATCH -t 0-00:30                         # Runtime in D-HH:MM format
 #SBATCH -p gpu
-#SBATCH --gres=gpu:2
-#SBATCH --mem=100                          # Memory total in MB (for all cores)
+#SBATCH --gres=gpu:4
+#SBATCH --mem=2G                           # Memory total in MB (for all cores)
 #SBATCH -o hostname_%j.out                 # File to which STDOUT will be written, including job ID
 #SBATCH -e hostname_%j.err                 # File to which STDERR will be written, including job ID
 #SBATCH --mail-type=ALL                    # Type of email notification- BEGIN,END,FAIL,ALL
@@ -12,7 +12,7 @@
 
 module load cuda/9.0
 
-source venv2/bin/activate
+source venv6/bin/activate
 
 python3 train_tcga.py config/tcga.yaml
 
